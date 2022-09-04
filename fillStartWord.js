7c9419da70560e97bdb45ff41e4baaebcdc28994
keaw <chatpimuk.klin@mail.ac.th>
2022-09-04 17:49:58 +0700

    FSW

--- fillStartWord.js
+++ fillStartWord.js
@@ -1,6 +1,14 @@
 const { template } = require('@babel/core')
 
 function fillStartWord(startWord, word) {
-  //code here
+  
+  let startWord = "kaew";
+
+  startWords.startWith("kaew");
+
+  let word = "za";
+  startWordss.startWith("za");
 }
-module.exports = fillStartWord
+//module.exports = fillStartWord
+
+console.log(fillStartWord("kaew","za"));
